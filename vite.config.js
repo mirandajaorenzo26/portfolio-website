c401438c6a2e290e2e8ea3044004d3dcd7b1703a
Jao Miranda <mirandajaorenzo@gmail.com>
2023-05-11 20:54:19 +0800

    Update package.json

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio-website',
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 });
