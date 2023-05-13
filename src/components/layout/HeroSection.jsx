3858d73703201d004fe85ef2e457cbc59b1ac09a
Jao Miranda <mirandajaorenzo@gmail.com>
2023-05-13 12:43:40 +0800

    Update design

--- src/components/layout/HeroSection.jsx
+++ src/components/layout/HeroSection.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 export default function HeroSection({ left: Left, right: Right }) {
   return (
-    <div className='my-5 md:items-center md:justify-evenly md:grid md:grid-cols-2 lg:grid-cols-3 '>
-      <div className="lg:col-span-2 ">
+    <div className='my-5 gap-5 md:grid md:grid-cols-2 md:items-center md:justify-evenly lg:grid-cols-3 '>
+      <div className='lg:col-span-2 '>
         <Left />
       </div>
       <div>
